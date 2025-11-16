bc00901dbd82065689a5a159b588b8d79ef9e58c
Lord-Abiolla <abiollajames@protonmail.com>
2025-11-16 23:18:10 +0300

    Set up navigation between pages

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import Link from "next/link";
+import Link from 'next/link';
 
 const Header: React.FC = () => {
     return (
-        <header className="max-w-4xl mx-auto p-4 mt-4 bg-gray-200 shadow rounded-full">
-            <nav className="flex justify-between">
-                <div className="ml-4">
-                    <h1 className="text-2xl font-bold text-blue-800">
-                        Blog
-                    </h1>
-                </div>
-                <div className="flex gap-4 mr-4 text-blue-800 font-semibold hover:text-blue-900 items-center">
-                    <Link href="/">Home</Link>
-                    <Link href="/posts">Posts</Link>
-                    <Link href="/users">Users</Link>
-                </div>
-            </nav>
+        <header className="bg-blue-600 text-white shadow-md py-4">
+            <div className="container mx-auto flex justify-between items-center px-4">
+                <h3 className="font-bold text-2xl">
+                    <Link href="/">Daily Contents</Link>
+                </h3>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li className="hover:underline">
+                            <Link href="/posts">Posts</Link>
+                        </li>
+                        <li className="hover:underline">
+                            <Link href="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
-    )
+    );
 }
 
 export default Header;
